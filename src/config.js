a17852b92dea9df6e9eafc9094651b0e310dd88d
Gabriel Ferreira <gabriel@lightup.com.br>
2018-09-26 16:14:46 -0300

    chore(config) add firebase config

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDTxAouPv1fiTRXTujeqtmI3tOqSJaew2c",
+    authDomain: "web-codeshare.firebaseapp.com",
+    databaseURL: "https://web-codeshare.firebaseio.com",
+    projectId: "web-codeshare",
+    storageBucket: "web-codeshare.appspot.com",
+    messagingSenderId: "809296150443"
+};
