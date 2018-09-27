6099a07fe33449290cab43ba6e6e6e2981acf2a3
Gabriel Ferreira <gabriel@lightup.com.br>
2018-09-27 10:07:25 -0300

    chore(etc) create config to router and redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { configureStore } from './redux-flow/stores/configure-store';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+const store = configureStore();
+
+const renderApp = Component => (
+    render(
+        <Provider store={store}>
+            <Router>
+                <Component />
+            </Router>
+        </Provider>, 
+        rootElement)
+);
+
+renderApp(App);
 registerServiceWorker();
