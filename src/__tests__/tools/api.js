3d78b40d0bbb35cbc52cb270c633c03e79631859
Gabriel Ferreira <gabriel@lightup.com.br>
2018-11-01 13:39:53 -0300

    fix api tests and remove firebase config and tests

---
+++ src/__tests__/tools/api.js
@@ -0,0 +1,20 @@
+import { expect } from 'chai';
+import api from 'tools/api';
+
+describe('Api Config', () => {
+  test('should be a object', () => {
+    expect(api).to.be.a('object');
+  });
+
+  test('should be have property url', () => {
+    expect(api).have.property('url');
+  });
+
+  test('url should be string', () => {
+    expect(api.url).to.be.a('string');
+  });
+
+  test('url should has value', () => {
+    expect(api.url).to.not.be.empty;
+  });
+});
--- src/tools/firebase.js
+++
@@ -1,6 +0,0 @@
-export default {
-  apiKey: 'AIzaSyBaE5ksQXzp9l66-0RjSlnXcSELxPcUvhI',
-  authDomain: 'dbfireshare.firebaseapp.com',
-  databaseURL: 'https://dbfireshare.firebaseio.com',
-  projectId: 'dbfireshare',
-};
